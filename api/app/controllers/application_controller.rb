8ff3548a04e50b777578ff178bd6b63ec7d4b303
Kurt Chan <ktc1016@Lenovo-PC.localdomain>
2022-10-22 10:01:07 +0800

    API: enable authentication

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-    # before_action :authenticate_user!
+    before_action :authenticate_user!
 
     def render_jsonapi_response(resource)
         if resource.errors.empty?
